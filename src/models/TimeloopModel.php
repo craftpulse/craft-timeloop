42f3255bd615e03b2dba656adb6499a2d7763fe7
michael <michael@dadamotion.be>
2021-06-05 17:26:53 +0200

    fixed an issue to create a new entry from scratch / fixed template fieldNames

--- src/models/TimeloopModel.php
+++ src/models/TimeloopModel.php
@@ -76,7 +76,11 @@ class TimeloopModel extends Model
     }
 
     public function init() {
-        $this->upcomingDates = Timeloop::$plugin->timeloop->getLoop($this, 2, true);
+
+            if ( $this->loopStartDate && $this->loopEndDate ) {
+                $this->upcomingDates = Timeloop::$plugin->timeloop->getLoop($this, 2, true);
+            }
+
     }
 
     /**
